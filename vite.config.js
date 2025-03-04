8c2b9bf68e90e79cadb107e97167531cf3de161e
Bhupendra <Bhupendra1430@bhu.ac.in>
2025-03-05 00:08:06 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  basename : "/x/",
   plugins: [
     react(),
     tailwindcss()
-  ],
-  basename : "/x/"
+  ]
 });
